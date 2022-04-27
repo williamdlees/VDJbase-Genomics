1960b311d7f21399c3a811270f3fc44931e8c79c
William Lees <william@lees.org.uk>
2022-04-27 10:51:49 +0100

    Update with latest code version

---
+++ annotate_all_samples.sh
@@ -0,0 +1,1 @@
+python ../digby_human_genomics/python/import_from_assemblies.py results/2022-04015_gene_lists/all_genes.csv data/2021-12-13_franken_gene_coords ../digby_human_genomics/ref results/2022-04015_gene_lists/all_annotations.csv
